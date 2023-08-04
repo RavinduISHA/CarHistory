8df1044802695d4dd9bdef566177f7970028ea78
DilshadMr360 <mohomeddilshad123@gmail.com>
2023-08-04 12:41:10 +0530

    home page updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,53 +1,249 @@
 @extends('layout')
 @section('content')
 
-<div class="container">
+<div style="border: 1px solid black;" class="">
 
-    <div class="flex justify-center  items-center h-screen">
+    <div class="container mx-auto">
 
-        <div class="w-1/2 px-4 md:px-8">
-            <h1 class="text-5xl font-bold text-blue-800 ">Why do u need to check Japanese Import Cars History </h1>
+        <div class="flex justify-center ">
+
+            <div class="w-1/2 px-4 md:px-8 my-16">
+                <h1 class="text-4xl font-bold text-blue-900 ">Why do u need to check Japanese Import Cars History </h1>
+
+            </div>
+
+            <div class="w-1/2 px-4 md:px-8 my-16">
+                <p class="text-md font-bold text-gray-500">Checking the history of Japanese import cars is essential for transparency and peace of mind.
+                    It verifies authenticity, reveals maintenance records, uncovers accidents and damages, prevents
+                    odometer fraud, identifies finance or lien issues, and addresses recalls or safety-related concerns.</p>
+
+            </div>
 
         </div>
 
-        <div class="w-1/2 px-4 md:px-8">
-            <p class="text-md font-bold text-black-300">Checking the history of Japanese import cars is essential for transparency and peace of mind.
-                It verifies authenticity, reveals maintenance records, uncovers accidents and damages, prevents
-                odometer fraud, identifies finance or lien issues, and addresses recalls or safety-related concerns.</p>
+        <div class="flex">
+            <div class="w-1/2 px-4 md:px-8">
+                <img src="{{ URL('images/Group_87.png')}}">
+                <h1 class="text-md font-bold text-blue-900">Authenticity</h1>
+                <p class="text-md font-bold text-blue-900">Verifying the car's history ensures that it is not stolen or involved in any illegal activities.</p>
+            </div>
+            <div class="w-0.5 h-13 bg-orange-500"></div>
 
+            <div class="w-1/2 px-4 md:px-8">
+                <img src="{{ URL('images/Group_88.png')}}">
+                <h1 class="text-md font-bold text-blue-900">Maintenance and Service</h1>
+                <p class="text-md font-bold text-blue-900">Understanding the vehicle's maintenance and service history helps assess its overall condition and potential maintenance needs.</p>
+            </div>
+            <div class="w-0.5 h-13 bg-orange-500"></div>
+
+            <div class="w-1/2 px-4 md:px-8">
+                <img src="{{ URL('images/Group_89.png')}}">
+                <h1 class="text-md font-bold text-blue-900">Accidents and Damages</h1>
+                <p class="text-md font-bold text-blue-900">History checks reveal any accident records or damages the car may have sustained, allowing you to assess its structural integrity and potential repairs.</p>
+            </div>
+            <div class="w-0.5 h-13 bg-orange-500"></div>
+
+            <div class="w-1/2 px-4 md:px-8">
+                <img src="{{ URL('images/Group_90.png')}}">
+                <h1 class="text-md font-bold text-blue-900">Recall Information</h1>
+                <p class="text-md font-bold text-blue-900">History checks provide information on any recalls or safety-related issues associated with the specific make and model of the imported car.</p>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="flex justify-center items-center my-10">
+        <div class="relative">
+            <img src="{{ URL('images/Group_93.png')}}" class="" alt="Your Image">
+            <div class="absolute bottom-10 left-20 w-full text-white space-y-4">
+                <p class="text-md font-bold text-orange-600">Preserving the Legacy</p>
+                <p class="text-md font-bold w-1/3">Uncovering the Past of Japanese Cars
+                    through an In-depth Sample Report</p>
+                <button class=" text-sm font-bold bg-orange-600 rounded-lg px-4 py-2">GET REPORT</button>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="flex items-center">
+        <img src="{{ URL('images/homecar.png')}}" class="w-full max-w-screen-lg" alt="Your Image">
+        <p class="text-2xl sm:text-3xl md:text-4xl font-bold text-blue-950 text-center my-4 sm:my-8">Why We Are the Premier Choice for Car Import History Enthusiasts</p>
+    </div>
+
+
+    <!--  NEW ROW -->
+
+    <div class="border-4 border-blue-950 mx-auto w-3/4 -my-60">
+
+
+        <!-- one row . -->
+        <div class="flex flex-col md:flex-row mt-20">
+
+            <!-- one box . -->
+            <div class="px-4 py-4 md:py-12 w-full md:w-6/12 ">
+
+                <div class="flex items-center">
+                    <h1 class="text-xl md:text-2xl font-extrabold text-orange-600">01</h1>
+                    <div class="border-t-2 border-blue-900 w-16 md:w-20 mx-2"></div>
+                    <div class="text-2xl md:text-4xl font-bold text-blue-900">Transparency</div>
+                </div>
+
+                <div class="w-full mt-4">
+                    <p class="text-xl md:text-2xl px-2 md:px-32">
+                        Ownership, accidents, mileage, service records. Transparent info aids buyers, revealing issues and ensuring informed decisions.
+                    </p>
+                </div>
+
+            </div>
+            <!-- box end -->
+
+            <!-- one box . -->
+            <div class="px-4 py-4 md:py-12 w-full md:w-6/12">
+
+                <div class="flex items-center">
+                    <h1 class="text-xl md:text-2xl font-extrabold text-orange-600">02</h1>
+                    <div class="border-t-2 border-blue-900 w-16 md:w-20 mx-2"></div>
+                    <div class="text-2xl md:text-4xl font-bold text-blue-900">Reliability</div>
+                </div>
+
+                <div class="w-full mt-4">
+                    <p class="text-xl md:text-2xl px-2 md:px-32">
+                        Reputable car history service offers reliable, accurate info on vehicle history, aiding buyers in assessing condition and reliability pre-purchase.
+                    </p>
+                </div>
+
+            </div>
+            <!-- box end -->
+
+        </div>
+        <!-- end row . -->
+
+        <!-- Repeat the same structure for other rows and boxes -->
+
+        <!--END NEW ROW -->
+
+        <!--  NEW ROW -->
+
+        <!-- one row . -->
+        <div class="flex flex-col md:flex-row">
+
+            <!-- one box . -->
+            <div class="px-4 py-4 md:py-12 w-full md:w-6/12">
+
+                <div class="flex items-center">
+                    <h1 class="text-xl md:text-2xl font-extrabold text-orange-600">03</h1>
+                    <div class="border-t-2 border-blue-900 w-16 md:w-20 mx-2"></div>
+                    <div class="text-2xl md:text-4xl font-bold text-blue-900">Peace of mind</div>
+                </div>
+
+                <div class="w-full mt-4">
+                    <p class="text-xl md:text-2xl px-2 md:px-32">
+                        Car history reports provide peace of mind by revealing crucial information, reducing the risk of hidden problems when buying a car.
+                    </p>
+                </div>
+
+            </div>
+            <!-- box end -->
+
+            <!-- one box . -->
+            <div class="px-4 py-4 md:py-12 w-full md:w-6/12">
+
+                <div class="flex items-center">
+                    <h1 class="text-xl md:text-2xl font-extrabold text-orange-600">04</h1>
+                    <div class="border-t-2 border-blue-900 w-16 md:w-20 mx-2"></div>
+                    <div class="text-2xl md:text-4xl font-bold text-blue-900">Verification</div>
+                </div>
+
+                <div class="w-full mt-4">
+                    <p class="text-xl md:text-2xl px-2 md:px-32">
+                        Reputable car history service offers reliable, accurate info on vehicle history, aiding buyers in assessing condition and reliability pre-purchase.
+                    </p>
+                </div>
+
+            </div>
+            <!-- box end -->
+
+        </div>
+        <!-- end row . -->
+
+        <!-- Repeat the same structure for other rows and boxes -->
+
+
+
+
+        <!--END NEW ROW -->
+
+
+        <!--  NEW ROW -->
+
+
+        <!-- one row . -->
+        <div class="flex flex-col md:flex-row">
+
+            <!-- one box . -->
+            <div class="px-4 py-4 md:py-12 w-full md:w-6/12">
+
+                <div class="flex items-center">
+                    <h1 class="text-xl md:text-2xl font-extrabold text-orange-600">05</h1>
+                    <div class="border-t-2 border-blue-900 w-16 md:w-20 mx-2"></div>
+                    <div class="text-2xl md:text-4xl font-bold text-blue-900">Negotiating power</div>
+                </div>
+
+                <div class="w-full mt-4">
+                    <p class="text-xl md:text-2xl px-2 md:px-32">
+                        Ownership, accidents, mileage, service records. Transparent info aids buyers, revealing issues and ensuring informed decisions.
+                    </p>
+                </div>
+
+            </div>
+            <!-- box end -->
+
+            <!-- one box . -->
+            <div class="px-4 py-4 md:py-12 w-full md:w-6/12">
+
+                <div class="flex items-center">
+                    <h1 class="text-xl md:text-2xl font-extrabold text-orange-600">06</h1>
+                    <div class="border-t-2 border-blue-900 w-16 md:w-20 mx-2"></div>
+                    <div class="text-2xl md:text-4xl font-bold text-blue-900">Customer Support</div>
+                </div>
+
+                <div class="w-full mt-4">
+                    <p class="text-xl md:text-2xl px-2 md:px-32">
+                        Reputable car history service offers reliable, accurate info on vehicle history, aiding buyers in assessing condition and reliability pre-purchase.
+                    </p>
+                </div>
+
+            </div>
+            <!-- box end -->
+
+        </div>
+        <!-- end row . -->
+
+        <!-- Repeat the same structure for other rows and boxes -->
+
+        <!--END NEW ROW -->
+    </div>
+
+
+
+    <!----->
+    <div style="border: 1px solid red;" class="relative">
+        <img src="{{ URL('images/homecar2.jpg')}}" class="" style="width: 100vw;" alt="Your Image">
+        <div class="absolute top-0 left-1/2 transform -translate-x-1/2 text-center">
+            <h1 class="text-blue-900 font-bold text-4xl p-4">What our Community is Saying</h1>
+            <img src="{{ URL('images/Group_94.png') }}" class="relative z-10 ">
+            <img src="{{ URL('images/Rectangle 144.png') }}" class="relative z-20 ">
+            <img src="{{ URL('images/arrow-left.png') }}">
+            <img src="{{ URL('images/arrow-right.png') }}">
+            <img src="{{ URL('images/Group_60.png') }}">
         </div>
 
     </div>
 
-    <div class="flex">
-        <div class="w-1/2 px-4 md:px-8">
-            <h1 class="text-md font-bold text-blue-800">Icon</h1>
-            <h1 class="text-md font-bold text-blue-800">Authenticity</h1>
-            <p class="text-md font-bold text-blue-800">Verifying the car's history ensures that it is not stolen or involved in any illegal activities.</p>
-        </div>
-        <div class="w-0.5 h-13 bg-orange-500"></div>
 
-        <div class="w-1/2 px-4 md:px-8">
-            <h1 class="text-md font-bold text-blue-800">Icon</h1>
-            <h1 class="text-md font-bold text-blue-800">Authenticity</h1>
-            <p class="text-md font-bold text-blue-800">Understanding the vehicle's maintenance and service history helps assess its overall condition and potential maintenance needs.</p>
-        </div>
-        <div class="w-0.5 h-13 bg-orange-500"></div>
 
-        <div class="w-1/2 px-4 md:px-8">
-            <h1 class="text-md font-bold text-blue-800">Icon</h1>
-            <h1 class="text-md font-bold text-blue-800">Authenticity</h1>
-            <p class="text-md font-bold text-blue-800">History checks reveal any accident records or damages the car may have sustained, allowing you to assess its structural integrity and potential repairs.</p>
-        </div>
-        <div class="w-0.5 h-13 bg-orange-500"></div>
 
-        <div class="w-1/2 px-4 md:px-8">
-            <h1 class="text-md font-bold text-blue-800">Icon</h1>
-            <h1 class="text-md font-bold text-blue-800">Authenticity</h1>
-            <p class="text-md font-bold text-blue-800">History checks provide information on any recalls or safety-related issues associated with the specific make and model of the imported car.</p>
-        </div>
 
-    </div>
 
 </div>
 
